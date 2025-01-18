040589329a6daa56488c054a40f37f2c60e11115
Mateusz Nalepa <mateuszcholyn@gmail.com>
2025-01-19 00:51:30 +0100

    commit

--- test-app/src/main/java/com/mateusznalepa/http/clients/util/logger/CustomLoggerWrapper.java
+++ test-app/src/main/java/com/mateusznalepa/http/clients/util/logger/CustomLoggerWrapper.java
@@ -3,7 +3,7 @@ package com.mateusznalepa.http.clients.util.logger;
 public class CustomLoggerWrapper {
 
     public static void log(String message) {
-        System.out.println(Thread.currentThread().getName() + " ### " + message);
+//        System.out.println(Thread.currentThread().getName() + " ### " + message);
     }
 
 }
--- test-app/src/main/kotlin/com/mateusznalepa/http/clients/TestAppConfig.kt
+++ test-app/src/main/kotlin/com/mateusznalepa/http/clients/TestAppConfig.kt
@@ -3,19 +3,16 @@ package com.mateusznalepa.http.clients
 object TestAppConfig {
     const val CPU_BOUND_SCHEDULER_ACTIVE = true
 
-    // Remember about: -Dio.netty.allocator.type=adaptive
-
     const val USE_DEDICATED_THREADS_FOR_CLIENT = false
     const val MOCK_SERVER_PORT = 8091
 
-//    val HTTP_CLIENTS = mapOf(20 to "l")
-    val HTTP_CLIENTS = mapOf(1 to "s")
+    val HTTP_CLIENTS = mapOf(20 to "l")
 
     fun printConfig() {
         println("Config")
         println("")
         println("CPU_BOUND_SCHEDULER_ACTIVE: $CPU_BOUND_SCHEDULER_ACTIVE")
-        println("CLIENT_MEMORY_ALLOCATOR_TYPE: ${System.getProperty("io.netty.allocator.type")}")
+        println("CLIENT_MEMORY_ALLOCATOR_TYPE: -Dio.netty.allocator.type=${System.getProperty("io.netty.allocator.type")}")
         println("USE_DEDICATED_THREADS_FOR_CLIENT: $USE_DEDICATED_THREADS_FOR_CLIENT")
         println("MOCK_SERVER_PORT: $MOCK_SERVER_PORT")
         println("HTTP_CLIENTS: $HTTP_CLIENTS")
